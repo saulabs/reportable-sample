217f7b775363ad59419451303abb4fb2dae40941
Martin Kavalar <martin@sauspiel.de>
2010-04-15 15:15:13 +0200

    rails3 skeleton app

---
+++ README.md
@@ -0,0 +1,6 @@
+rails3 reportable sample project
+==========
+
+See [http://saulabs.net/projects/reportable][0]
+
+[0]:http://saulabs.net/projects/reportable
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '202cc65850b5ce9da8880335217b9d28508296dd404ef961a3ca1acc6fdac4fcb9824c858d3691dd1fa18b27d9124b72e8a6107626e772cd22b7046c4a940186'
